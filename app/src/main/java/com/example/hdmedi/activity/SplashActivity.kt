d15ff0f7268e27f47a815b8ff0a2cf93704b74e4
SeungGu Kang <alex9366@naver.com>
2023-09-12 21:51:59 +0900

    Fix. 스플래쉬 화면이 끝나면 온보딩 화면으로 이동하도록 수정

--- app/src/main/java/com/example/hdmedi/activity/SplashActivity.kt
+++ app/src/main/java/com/example/hdmedi/activity/SplashActivity.kt
@@ -7,17 +7,14 @@ import android.os.Handler
 import com.example.hdmedi.R
 
 class SplashActivity : AppCompatActivity() {
-
     private val splash_delay : Long = 2000
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
-
         Handler().postDelayed({
-            val intent = Intent(this@SplashActivity, LoginActivity::class.java)
+            val intent = Intent(this@SplashActivity, OnBoardingActivity::class.java)
             startActivity(intent)
             finish()
         }, splash_delay)
