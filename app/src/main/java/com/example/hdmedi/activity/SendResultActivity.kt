32f9e6025b9e7df04f021746f0d95887b3ab3c03
SeungGu Kang <alex9366@naver.com>
2023-09-15 19:00:41 +0900

    Feat. 검사 결과 UI 구현

---
+++ app/src/main/java/com/example/hdmedi/activity/SendResultActivity.kt
@@ -0,0 +1,12 @@
+package com.example.hdmedi.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.hdmedi.R
+
+class SendResultActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_send_result)
+    }
+}
