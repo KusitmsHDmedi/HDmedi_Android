df1f5ec17eef7f7fd61cc942de0fac725dabf887
SeungGu Kang <alex9366@naver.com>
2023-09-16 20:06:17 +0900

    Fix. MainActivity를 HomeActivity로 이름 수정

--- app/src/main/java/com/example/hdmedi/activity/HomeActivity.kt
+++ app/src/main/java/com/example/hdmedi/activity/HomeActivity.kt
@@ -1,11 +1,10 @@
 package com.example.hdmedi.activity
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.hdmedi.R
 import com.example.hdmedi.databinding.ActivityMainBinding
 
-class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
+class HomeActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
