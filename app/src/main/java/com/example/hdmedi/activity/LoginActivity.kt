541a9c9635660feeb0ec4e9d55e5f5e75baa4a77
ShinKing <ssk7750@naver.com>
2023-09-17 03:51:40 +0900

    Feat. login Token

--- app/src/main/java/com/example/hdmedi/activity/LoginActivity.kt
+++ app/src/main/java/com/example/hdmedi/activity/LoginActivity.kt
@@ -6,13 +6,25 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import com.example.hdmedi.R
+import com.example.hdmedi.model.SignInResponseBody
+import com.example.hdmedi.retrofit.APIS
+import com.example.hdmedi.retrofit.RetrofitInstance
+import com.example.hdmedi.sharedPreference.MyApplication
 import com.navercorp.nid.NaverIdLoginSDK
 import com.navercorp.nid.oauth.NidOAuthLogin
 import com.navercorp.nid.oauth.OAuthLoginCallback
 import com.navercorp.nid.profile.NidProfileCallback
 import com.navercorp.nid.profile.data.NidProfileResponse
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.create
 
 class LoginActivity : AppCompatActivity() {
+
+
+    private val APIS = RetrofitInstance.retrofitInstance().create<APIS>(APIS::class.java)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
@@ -31,9 +43,44 @@ class LoginActivity : AppCompatActivity() {
                     val naverAccessToken = NaverIdLoginSDK.getAccessToken()
                     Log.e("login", "naverAccessToken : $naverAccessToken")
 
-                    Intent(this@LoginActivity, ParentsSettingActivity::class.java).apply {
-                        startActivity(this)
+
+                    val naverToken = "Bearer $naverAccessToken"
+
+                    try{
+                        APIS.postNaverToken(naverToken).enqueue(
+                            object : Callback<SignInResponseBody> {
+
+                                override fun onResponse(call: Call<SignInResponseBody>, response: Response<SignInResponseBody>) {
+                                    if (response.isSuccessful) {
+
+                                        val accessToken = response.body()!!.data.accessToken
+                                        val userId = response.body()!!.data.userId
+
+                                        MyApplication.preferences.setString("accessToken", accessToken)
+                                        MyApplication.preferences.setString("userId", userId.toString())
+
+
+                                        Log.d("accessToken", accessToken)
+
+                                        //화면 이동
+                                        Intent(this@LoginActivity, ParentsSettingActivity::class.java).apply {
+                                            startActivity(this)
+                                        }
+
+                                            } else {
+                                        Log.d("SignInResponseBody Response : ", " fail 1 , ${response.message()}")
+                                          }
+                                }
+
+                                override fun onFailure(call: Call<SignInResponseBody>, t: Throwable) {
+                                    Log.d("SignInResponseBody Response : ", " fail 2 , ${t.message.toString()}")
+                                }
+                            })
+                    } catch (e:Exception) {
+                        Log.d("SignInResponseBody response : ", " fail 3 , ${e.message}")
                     }
+
+
                 }
 
                 override fun onError(errorCode: Int, message: String) {
--- app/src/main/java/com/example/hdmedi/retrofit/APIS.kt
+++ app/src/main/java/com/example/hdmedi/retrofit/APIS.kt
@@ -1,4 +1,23 @@
 package com.example.hdmedi.retrofit
 
+import android.telecom.Call
+import com.example.hdmedi.model.SignInRequestBody
+import com.example.hdmedi.model.SignInResponseBody
+import retrofit2.http.Body
+import retrofit2.http.Header
+import retrofit2.http.POST
+import retrofit2.http.Query
+
+
 interface APIS {
+
+
+    //로그인
+    @POST("api/user/signIn")
+    fun postNaverToken(
+
+        @Header("accessToken") accessToken : String
+    ): retrofit2.Call<SignInResponseBody>
+
+
 }
