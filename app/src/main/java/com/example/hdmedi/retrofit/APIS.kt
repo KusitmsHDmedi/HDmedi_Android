3d18cfc2dc5ed810bb62a92de7ca972ed513c725
ShinKing <ssk7750@naver.com>
2023-09-17 05:12:51 +0900

    Feat. API 추가

--- app/src/main/java/com/example/hdmedi/retrofit/APIS.kt
+++ app/src/main/java/com/example/hdmedi/retrofit/APIS.kt
@@ -1,11 +1,9 @@
 package com.example.hdmedi.retrofit
 
 import android.telecom.Call
-import com.example.hdmedi.model.SignInRequestBody
-import com.example.hdmedi.model.SignInResponseBody
-import com.example.hdmedi.model.SignUpRequestBody
-import com.example.hdmedi.model.SignUpResponseBody
+import com.example.hdmedi.model.*
 import retrofit2.http.Body
+import retrofit2.http.GET
 import retrofit2.http.Header
 import retrofit2.http.POST
 import retrofit2.http.Query
@@ -21,11 +19,31 @@ interface APIS {
         @Body signInRequestBody: SignInRequestBody
     ): retrofit2.Call<SignInResponseBody>
 
+    //회원가입
     @POST("api/user/signUp")
     fun postSignUp(
         @Header ("Authorization") Authorization : String,
         @Body signUpRequestBody: SignUpRequestBody
     ) : retrofit2.Call<SignUpResponseBody>
 
+    //인증 코드 발급
+    @GET("api/user/authCode")
+    fun getAuthCode(
+        @Header ("Authorization") Authorization : String,
+    ) : retrofit2.Call<AuthCodeResponseBody>
 
+    //선생님 로그인
+    @POST("api/user/signIn/guest")
+    fun postGuestSignIn(
+        @Header ("Authorization") Authorization : String,
+
+        ) : retrofit2.Call<GuestSignInResponseBody>
+
+    //설문 조사 제출
+    @POST("api/survey")
+    fun postSurvey(
+        @Header ("Authorization") Authorization : String,
+
+        @Body surveyRequestBody: SurveyRequestBody
+        ) : retrofit2.Call<SurveyResponseBody>
 }
