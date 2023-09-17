1a9247c94c162909bcd1b53c5003c19abc493fc2
SeungGu Kang <alex9366@naver.com>
2023-09-17 14:32:19 +0900

    Fix. resultText에 자녀의 이름이 표시되도록 수정

--- app/src/main/java/com/example/hdmedi/activity/SendResultActivity.kt
+++ app/src/main/java/com/example/hdmedi/activity/SendResultActivity.kt
@@ -1,14 +1,20 @@
 package com.example.hdmedi.activity
 
 import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
+import android.text.SpannableStringBuilder
+import android.text.Spanned
+import android.text.style.ForegroundColorSpan
 import com.example.hdmedi.R
 import com.example.hdmedi.databinding.ActivitySendResultBinding
+import com.example.hdmedi.sharedPreference.MyApplication
 
 class SendResultActivity : BaseActivity<ActivitySendResultBinding>(R.layout.activity_send_result) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         initButton()
+        initResultText()
     }
 
     private fun initButton(){
@@ -19,6 +25,16 @@ class SendResultActivity : BaseActivity<ActivitySendResultBinding>(R.layout.acti
                 startActivity(this)
             }
             finish()
+        }
+    }
+
+    private fun initResultText(){
+        val greenSpan = ForegroundColorSpan(Color.parseColor("#2BAE76"))
+        val name = MyApplication.preferences.getString("childrenName", "")
+        binding.resultText.text = name + "의 자가진단을\n선생님께 요청했습니다!"
+        val textData = binding.resultText.text.toString()
+        binding.resultText.text = SpannableStringBuilder(textData).apply {
+            setSpan(greenSpan, 0, name.length, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
         }
     }
 }
